f683e7aec893fbff8c91d82269b00da25efe59db
f-toni <toniolofiliberto@gmail.com>
2023-10-04 16:03:39 +0200

    Aggiunto /me (user+iban)

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Response, Request } from "express";
 import { User } from './user.model';
+import { BankAccount } from "../bank-account/bank-account.model";
 
 export let usersList = async (req: Request, res: Response, next: NextFunction) => {
   let usersList = await User.find();
@@ -7,5 +8,7 @@ export let usersList = async (req: Request, res: Response, next: NextFunction) =
 }
 
 export let me = async (req: Request, res: Response, next: NextFunction) => {
-  res.json(req.user!);
+  const bankAccount = await BankAccount.findOne({ user: req.user.id });
+  const user = await User.findOne({ _id: req.user.id });
+  res.json({iban: bankAccount.iban, user});
 }
